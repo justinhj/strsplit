432895e303d4beed1b32608a5dd763679f34e211
Justin Heyes-Jones <justinhj@gmail.com>
2021-12-21 22:51:28 -0800

    step 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+#![warn(rust_2018_idioms)]
+
+#[derive(Debug)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str
+}
+
+impl <'a>StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        StrSplit{
+                remainder: haystack,
+                delimiter}
+    }
+}
+
+impl <'a>Iterator for StrSplit<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+           let until_next = &self.remainder[..next_delim];
+           self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+           Some(until_next)
+        } else {
+            self.remainder = "";
+            None
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Yes");
+
+    let input = StrSplit::new("a b c d empty full poop", " ");
+
+    let words = input.into_iter();
+
+    for word in words {
+        println!("Word! {}", word);
+    }
 }
