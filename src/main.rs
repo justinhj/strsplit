47bcef90ee4153a69b798b6d8cff686273e075c5
Justin Heyes-Jones <justinhj@gmail.com>
2021-12-22 17:17:36 -0800

    ?

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-#![warn(rust_2018_idioms)]
+#![warn(rust_2018_idioms, dead_code)]
 
 #[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str
 }
 
 impl <'a>StrSplit<'a> {
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         StrSplit{
-                remainder: haystack,
+                remainder: Some(haystack),
                 delimiter}
     }
 }
@@ -18,22 +18,23 @@ impl <'a>Iterator for StrSplit<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delim) = self.remainder.find(self.delimiter) {
-           let until_next = &self.remainder[..next_delim];
-           self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-           Some(until_next)
-        } else if !self.remainder.is_empty() {
-            let rest = self.remainder;
-            self.remainder = "";
-            Some(rest)
-        } else {
-            None
+        match self.remainder {
+            None => None,
+            Some(remainder) => {
+                if let Some(next_delim) = remainder.find(self.delimiter) {
+                   let until_next = &remainder[..next_delim];
+                   self.remainder = Some(&remainder[(next_delim + self.delimiter.len())..]);
+                   Some(until_next)
+                } else {
+                    self.remainder = None;
+                    Some(remainder)
+                }
+            }
         }
     }
 }
 
 fn main() {
-
     let input = StrSplit::new("a b c d e ", " ");
     let words = input.into_iter();
 
